72746e8eef76f2bd37e53e8b03b67b69f2a06dce
wa1tnr <wa1tnr@users.noreply.github.com>
2021-02-07 19:09:43 +0000

    Nice little program here.
    Sends a group of three characters, repeatedly,
    with a slow enough delay to appreciate it.
    
    Meant as a test of the ability to add custom
    output to the program, prior to working on
    input (keyboard acquisition into the USART).
    
    From the factory example set, as baseline.
    
    	modified:   hello_uart.c
    
    On branch dvlp-aa-

--- 03-usart.d/pico-examples/uart/hello_uart/hello_uart.c
+++ 03-usart.d/pico-examples/uart/hello_uart/hello_uart.c
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+// wa1tnr
+// Sun Feb  7 18:46:13 UTC 2021
 
 #include <stdio.h>
 #include "pico/stdlib.h"
@@ -18,6 +20,21 @@
 // datasheet for information on which other pins can be used.
 #define UART_TX_PIN 0
 #define UART_RX_PIN 1
+
+void looper() {
+    // see if the loop is called as expected before trying for any input.
+    uart_putc(UART_ID, 'h'); // use new test chars
+    uart_putc(UART_ID, 'i');
+    uart_putc(UART_ID, 'j');
+    uart_putc(UART_ID, ' ');
+    // larger delay - empty loop volatile int won't be optimized out
+    // suspiciously fast now - these should take a finite time to complete - not sure that's true
+    // lets just look it up - what they recommend for a delay in a loop
+    // of course just used it yesterday in blink.c
+    // sleep_ms(250);
+    for (volatile int i=2299338871;i>0;i--) { } // keepsake code - nonfunctioning ;)
+    sleep_ms(250); // works a treat as they say - nice calm repeated group of 3 chars
+}
 
 int main() {
     // Set up our UART with the required speed.
@@ -39,6 +56,11 @@ int main() {
 
     // Send out a string, with CR/LF conversions
     uart_puts(UART_ID, " Hello, UART!\n");
+
+    // locally authored payload - a loop:
+    while(1) {
+        looper(); // called once and ran once ask asked ;)
+    }
 }
 
 /// \end::hello_uart[]
